da95079d91c6f7cf31d259a83ed204debc511fa2
rakesh singh bhandari <rakeshsinghbhandari252002@gmail.com>
2026-07-22 13:39:59 +0530

    footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           {/* Logo */}
           <div>
             <h2 className="text-3xl font-bold text-blue-400">
-          yyyygsyasgysgwy Electronic
+          MMMMMMMMMMMMMMMMMMMMMMMMM Electronic
             </h2>
 
             <p className="mt-4 text-gray-400">
